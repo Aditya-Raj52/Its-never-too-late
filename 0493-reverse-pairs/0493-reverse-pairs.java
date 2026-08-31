ea9d5c417e622e23fe5d48ea343087654b9138d2
Aditya <rajaditya9205@gmail.com>
2026-09-01 02:06:58 +0530

    Time: 40 ms (97.62%), Space: 60.3 MB (68.09%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,64 +1,48 @@
 class Solution {
-    int ans=0;
-    public int reversePairs(int[] nums) {        
-        int n = nums.length;
-        reversePairs(nums,0,n);
-        return ans;
+    public int reversePairs(int[] nums) {
+        if (nums == null || nums.length <= 1) return 0;
+        int[] temp = new int[nums.length];
+        return mergeSortCount(nums, temp, 0, nums.length - 1);
     }
 
-    public int[] reversePairs(int[] nums,int str, int end) {  
-        if(end-str==1){
-            return new int[]{nums[str]};
+    private static int mergeSortCount(int[] arr, int[] temp, int left, int right) {
+        if (left >= right) return 0;
+
+        int mid = left + (right - left) / 2;
+        int count = mergeSortCount(arr, temp, left, mid) + 
+                    mergeSortCount(arr, temp, mid + 1, right);
+
+        // Step 1: Count reverse pairs between left half and right half
+        int j = mid + 1;
+        for (int i = left; i <= mid; i++) {
+            while (j <= right && (long) arr[i] > 2L * arr[j]) {
+                j++;
+            }
+            count += (j - (mid + 1));
         }
-        int mid = (str+end)/2;
-        int[] srtArr = new int[end-str];
-        int[] left = reversePairs(nums,str,mid);
-        int[] right = reversePairs(nums,mid,end);
-        int p1=0;
-        int p2=0;
-        int cnt=0;
-        int m =left.length;
-        int n= right.length;
-        while(p1<m && p2<n){
-            if(left[p1]>2L*right[p2]){
-                cnt+=m-p1;
-                p2++;
-            }
-            else{
-                p1++;
-            }
-        }
-        ans+=cnt;
-        p1=0;
-        p2=0;
-        cnt=0;
-        while(p1<m && p2<n){
-            if(left[p1]<=right[p2]){
-                srtArr[cnt]=left[p1];
-                p1++;
-                cnt++;
-            }
-            else{
-                srtArr[cnt]=right[p2];
-                p2++;
-                cnt++;
-            }
-        }
-        if(p1==m){
-            while(p2<n){
-                srtArr[cnt]=right[p2];
-                cnt++;
-                p2++;
-            }
-        }
-        else{
-            while(p1<m){
-                srtArr[cnt]=left[p1];
-                cnt++;
-                p1++;
+
+        // Step 2: Standard merge of two sorted halves
+        merge(arr, temp, left, mid, right);
+
+        return count;
+    }
+
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left, j = mid + 1, k = left;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
             }
         }
 
-        return srtArr;
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
     }
 }
